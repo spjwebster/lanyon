b59c542b920347de0f9474e15a04c119d64be21d
Steve Webster <spjwebster@gmail.com>
2013-01-18 09:44:48 +0000

    Don't die silently on YAML front matter errors
    Need to find a better way to handle this ultimately, but for now I'd prefer seeing the exception details from pyyaml rather than complete silence.

--- lanyon/pre_processors.py
+++ lanyon/pre_processors.py
@@ -25,33 +25,25 @@ class YAMLFrontMatterLoader( SitePreProcessor ):
             if not ( self.config.has_key( 'yaml_extensions' ) and node.extension in self.config[ 'yaml_extensions' ] ):
                 return
 
-            try:
-                # Open up content file for node
-                content_path = os.path.join( self.config[ 'content_path' ], node.path )
-                content_file = codecs.open( content_path, 'r', 'utf-8' )
-                
-                # Peek at the file to see if it might contain YAML front matter
-                if content_file.read( 3 ) == "---":
-                    # Read in file content
-                    content_file.seek( 0 )
-                    content = content_file.read()
+            # Open up content file for node
+            content_path = os.path.join( self.config[ 'content_path' ], node.path )
+            content_file = codecs.open( content_path, 'r', 'utf-8' )
+            
+            # Peek at the file to see if it might contain YAML front matter
+            if content_file.read( 3 ) == "---":
+                # Read in file content
+                content_file.seek( 0 )
+                content = content_file.read()
 
-                    # Extract YAML data into node data
-                    match = re.match( self.front_matter_re, content )
-                    if match:
-                        content = content[match.end():]
-                        yaml_content = match.group( 1 )
-                        try:
-                            node.data.update( yaml.load( yaml_content ) )
-                        except:
-                            # TODO: Handle malformed YAML data
-                            pass
-            except:
-                # TODO: Handle file read error
-                pass
-                
-            finally:
-                content_file.close()
+                # Extract YAML data into node data
+                match = re.match( self.front_matter_re, content )
+                if match:
+                    content = content[match.end():]
+                    yaml_content = match.group( 1 )
+                    node.data.update( yaml.load( yaml_content ) )
+            
+            # Done with this file
+            content_file.close()
                 
 class BlogPostProcessor( SitePreProcessor ):
     tag_slug_re = re.compile("[^a-z0-9-]", re.IGNORECASE )
